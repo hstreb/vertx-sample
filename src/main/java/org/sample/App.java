c138ca88cfce6c5ca8d05720b8a922cf9df12d4b
Humberto Streb <hstreb@gmail.com>
2016-07-24 19:34:25 -0300

    Added support to metrics

--- src/main/java/org/sample/App.java
+++ src/main/java/org/sample/App.java
@@ -2,18 +2,30 @@ package org.sample;
 
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.JksOptions;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.auth.jwt.JWTOptions;
+import io.vertx.ext.dropwizard.DropwizardMetricsOptions;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.JWTAuthHandler;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class App {
+
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     public static void main(String[] args) {
-        Vertx vertx = Vertx.vertx();
+        System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.Log4j2LogDelegateFactory");
+        logger.info("start app");
+        Vertx vertx = Vertx.vertx(new VertxOptions().setMetricsOptions(
+                new DropwizardMetricsOptions().setEnabled(true)
+        ));
         vertx.createHttpServer(getOptions())
                 .requestHandler(getRouter(vertx)::accept)
                 .listen();
@@ -42,11 +54,14 @@ public class App {
         return req -> {
             String username = req.request().getParam("username");
             String password = req.request().getParam("password");
+            logger.info("try to login user: {}, {}", username, req.toString());
             if ("humberto".equals(username) && "secret".equals(password)) {
+                logger.info("login success");
                 String token = jwt.generateToken(new JsonObject().put("sub", "humberto"), new JWTOptions().setExpiresInSeconds(60L));
                 req.response().putHeader("Content-Type", "text/plain");
                 req.response().end(token);
             } else {
+                logger.info("login failure!");
                 req.fail(401);
             }
         };
